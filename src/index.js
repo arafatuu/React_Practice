5c2f79c0404acfd603dd94ecb7466b8a6c42fc02
Arafat Rahman <atrahmanbd5@gmail.com>
2021-06-23 14:12:00 +0600

    Representation of JSX object

--- src/index.js
+++ src/index.js
@@ -48,28 +48,64 @@ import ReactDOM from 'react-dom';
 
 
 
-// 04. JSX itself an expression
+// // 04. JSX itself an expression
 
-function concatName(user){
-    return user.fName+' '+user.lName;
-}
-function check(user)
-{
-    if(user)
-    return <h1> Hello {concatName(user)}</h1>;
-    else
-    return <h1>Hello Stanger</h1>
-}
+// function concatName(user){
+//     return user.fName+' '+user.lName;
+// }
+// function check(user)
+// {
+//     if(user)
+//     return <h1> Hello {concatName(user)}</h1>;
+//     else
+//     return <h1>Hello Stanger</h1>
+// }
 
-const user = {
-    fName: "Arafatt",
-    lName: "Rahman"
-}
+// const user = {
+//     fName: "Arafatt",
+//     lName: "Rahman"
+// }
 
-const element = check(user);
+// const element = check(user);
 
 
-ReactDOM.render(
-    element,
-    document.getElementById('root')
-)
+// ReactDOM.render(
+//     element,
+//     document.getElementById('root')
+// )
+
+
+//05. JSX represents Objects
+
+   //method 01
+
+   const element1 = (
+     <h1 className="ok">
+         Hello Baby....! Whassuphhh.....I am from Ok
+     </h1>
+   );
+   
+
+   //Method 02:
+
+   const element2 = React.createElement(
+       'h1',
+       {className: 'okok'},
+       'Hello babby,,, I am from okok'
+
+   );
+
+
+   //method 03:
+
+   const element3 = {
+       type: 'h1',
+       props: {
+           className: 'OKOKOK',
+           children: 'Babyyy..... I am from OKOKOK'
+       }
+   }
+
+
+   ReactDOM.render(element1,document.getElementById('root'));
+   
