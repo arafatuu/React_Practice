2dfbac5666b32b7f374d7ee9712adeb3c092e33e
Arafat Rahman <atrahmanbd5@gmail.com>
2021-06-23 13:49:07 +0600

    Another example of embadding elemnt in JSX

--- src/index.js
+++ src/index.js
@@ -13,15 +13,32 @@ import ReactDOM from 'react-dom';
 
 
 
-//02. Embedding expression in JSX
-const name = "Arfat Rahman";
-const element = <h1>Hello {name}</h1>;
+// //02. Embedding expression in JSX
+// const name = "Arfat Rahman";
+// const element = <h1>Hello {name}</h1>;
+// ReactDOM.render(
+//     element,
+//     document.getElementById('root')
+// )
+
+
+//03.Another Embedding expression in JSX
+function concatName(user){
+    return user.fName+' '+user.lName;
+}
+
+const user = {
+    fName: "Arafat",
+    lName: "Rahman"
+}
+
+const element = <h1>Hello {concatName(user)}</h1>;
+
+
 ReactDOM.render(
     element,
     document.getElementById('root')
 )
-
-
 
 
 
