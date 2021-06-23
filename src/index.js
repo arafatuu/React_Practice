80fd95eb6b5b272ddbd607733d65815b9d2865a0
Arafat Rahman <atrahmanbd5@gmail.com>
2021-06-23 13:42:45 +0600

    Embedding expression in JSX

--- src/index.js
+++ src/index.js
@@ -2,18 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-//01.Introducing JSX
+// //01.Introducing JSX
 
-const element = <h1>Hello Bangladesh</h1>;
+// const element = <h1>Hello Bangladesh</h1>;
+// ReactDOM.render(
+//     element,
+//     document.getElementById('root')
+// )
+
+
+
+
+//02. Embedding expression in JSX
+const name = "Arfat Rahman";
+const element = <h1>Hello {name}</h1>;
 ReactDOM.render(
     element,
     document.getElementById('root')
 )
-
-
-
-
-
 
 
 
